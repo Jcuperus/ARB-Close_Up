8fc6238c698b0ba8ef5516b67afcf60672cde02e
Jaep Cuperus <jaepcuperus@hotmail.com>
2016-11-08 11:47:41 +0100

    Network debug

---
+++ Assets/networkDebug.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class networkDebug : NetworkManager{
+
+    public override void OnServerConnect(NetworkConnection conn) {
+        Debug.Log("conn: " + conn.address);
+    }
+}
