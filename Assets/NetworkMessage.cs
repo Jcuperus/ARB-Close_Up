e1fff736c30e614bcf166f4e61cc5d8cd3908cd2
Jaep Cuperus <jaepcuperus@hotmail.com>
2016-11-09 11:43:24 +0100

    NOOOOOOOOOOO

---
+++ Assets/NetworkMessage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkMessage : MessageBase {
+    public string message;
+
+    public NetworkMessage(string message) {
+        this.message = message;
+    }
+}
