b5540163a8e107bb76fb8a2e2265438105694026
Matthijs Bergsma <matthijs.bergsma@live.nl>
2016-11-11 13:13:40 +0100

    button behaviour order fix

--- Assets/StartButtonBehaviour.cs
+++ Assets/StartButtonBehaviour.cs
@@ -4,10 +4,9 @@ using UnityEngine.UI;
 
 public class StartButtonBehaviour : MonoBehaviour {
     void Awake() {
+        NetworkManagerScript networkManagerScript = (NetworkManagerScript)GameObject.Find("NetworkManager").GetComponent(typeof(NetworkManagerScript));
         Button joinButton = GameObject.Find("StartButton").GetComponent<Button>();
         joinButton.onClick.AddListener(networkManagerScript.handleStart);
-
-        NetworkManagerScript networkManagerScript = (NetworkManagerScript)GameObject.Find("NetworkManager").GetComponent(typeof(NetworkManagerScript));
         this.gameObject.SetActive(networkManagerScript.getIsHost());
     }
 }
