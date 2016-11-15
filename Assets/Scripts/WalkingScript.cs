eb29746b78ee0277b84e2ff9a5b7793beac36d8b
Matthijs Bergsma <matthijs.bergsma@live.nl>
2016-11-15 14:11:16 +0100

    walk script af

--- Assets/Scripts/WalkingScript.cs
+++ Assets/Scripts/WalkingScript.cs
@@ -5,31 +5,37 @@ public class WalkingScript : MonoBehaviour {
     public int speed = 60;
     public int directionChangeTimer = 3;
     public bool isMoving = true;
-    private Vector3 rotation;
+    private Vector3 rotation = new Vector3();
 
 	// Use this for initialization
 	void Awake () {
-        //rotation.Set(0,Random.Range(0, 360),0);
+        rotation.Set(0,Random.Range(0, 360),0);
         StartCoroutine(changeDirection());
     }
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(new Vector3(speed*Time.deltaTime,0,0));
+        if(isMoving) {
+            this.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
     }
 
     void OnTriggerEnter(Collider other) {
-        Debug.Log("collision");
-        float newRotation = rotation.y + 180;
-        rotation.Set(0, newRotation, 0);
-        this.transform.Rotate(rotation);
+        //Debug.Log("collision enter: "+other.name);
+        if(other.GetComponent<Collider>().gameObject.layer != LayerMask.NameToLayer("Objectives")) {
+            this.transform.Rotate(new Vector3(0, 180, 0));
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        //Debug.Log("collision leave: "+other.name);
     }
 
     private IEnumerator changeDirection() {
         while(isMoving) {
             yield return new WaitForSeconds(directionChangeTimer);
-            //rotation.Set(0, Random.Range(0, 360), 0);
-            //this.transform.Rotate(rotation);
+            rotation.Set(0, Random.Range(0, 360), 0);
+            this.transform.Rotate(rotation);
         }
     }
 }
