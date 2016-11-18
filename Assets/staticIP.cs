548b86fd904414dedf0645561db3a996752ee3af
Matthijs Bergsma <matthijs.bergsma@live.nl>
2016-11-18 16:02:51 +0100

    static IP

---
+++ Assets/staticIP.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class staticIP : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Text>().text = "192.168.43.1";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
